24aea809e923f8040e06c65811028a93cb64c02b
Gabe Gorelick <gabegorelick@gmail.com>
2011-04-17 01:21:51 -0400

    Initial commit

---
+++ src/edu/umd/cs/guitar/replayer/test/ReplayerTest.java
@@ -0,0 +1,29 @@
+package edu.umd.cs.guitar.replayer.test;
+
+import org.junit.Test;
+
+import edu.umd.cs.guitar.replayer.SWTReplayer;
+import edu.umd.cs.guitar.replayer.SWTReplayerConfiguration;
+import edu.umd.cs.guitar.ripper.SWTApplicationRunner;
+import edu.umd.cs.guitar.ripper.test.aut.SWTMultiWindowDynamicApp;
+
+
+public class ReplayerTest {
+
+	@Test
+	public void testReplayer() {
+		SWTReplayerConfiguration config = new SWTReplayerConfiguration();
+		
+		String autName = SWTMultiWindowDynamicApp.class.getSimpleName();
+		
+		config.setMainClass(SWTMultiWindowDynamicApp.class.getName());
+		config.setGuiFile("guistructures/" + autName + ".GUI.xml");
+		config.setEfgFile("efgs/" + autName + ".EFG.xml");
+		config.setTestcase("testcases/" + autName + "/t_e1819273420_e1819273420_e1819273420_e1819273420_e1819273420.tst");
+		
+		Thread guiThread = Thread.currentThread();
+		final SWTReplayer replayer = new SWTReplayer(config, guiThread);
+		
+		new SWTApplicationRunner(replayer).run();		
+	}
+}
